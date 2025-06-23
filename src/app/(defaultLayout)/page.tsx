0a1910be8f35d57959cbaad1850796485df3a027
Adeepa Gunathilake <iceblob@proton.me>
2025-06-24 00:43:49 +0530

    Add HeroBanner3 component and update styles for improved layout

--- src/app/(defaultLayout)/page.tsx
+++ src/app/(defaultLayout)/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import HeroBanner2 from "../Components/HeroBanner/HeroBanner2";
 import About1 from "../Components/About/About1";
 import Services2 from "../Components/Services/Services2";
 import Project2 from "../Components/Project/Project2";
 import Process2 from "../Components/Process/Process2";
 import Faq1 from "../Components/Faq/Faq1";
+import HeroBanner3 from "../Components/HeroBanner/HeroBanner3";
 const page = () => {
   return (
     <div>
-      <HeroBanner2></HeroBanner2> {/* adeepa */}
+      <HeroBanner3></HeroBanner3> {/* adeepa */}
       <About1></About1> {/* adeepa */}
       <Services2></Services2> {/* hasala */}
       <Project2></Project2> {/* tharindu */}
